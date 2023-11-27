18fea4fa74303ea201f9c5841b5bafb1f7ad7bf5
Hakim-Latroch <ahakim.latroch@gmail.com>
2023-11-27 07:29:45 +0100

    Finished 2 Tasks

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from . import util
+
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+def entry(request, title):
+    entry = util.get_entry(title=title)
+    return render(request, "encyclopedia/title.html",{
+        "entry": entry,
+        "title":f"{title}",
+        "notFound":entry == None
+    })
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST.get("q")  
+        entries = util.list_entries()  
+        if  query.lower() in  map(str.lower, entries):
+            return HttpResponseRedirect(reverse("wiki:entry", args=[query]))  
+        else:
+            return render(request, "encyclopedia/title.html", {
+                "title": query,
+                "notFound": True
+            })
+    
+
+
